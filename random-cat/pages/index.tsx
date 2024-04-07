900dd9b43c3624963be0401761f9367dfead0239
kuboyumi <121540707+kuboyumi@users.noreply.github.com>
2024-04-08 01:33:46 +0900

    fix text in button (#6)

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -24,7 +24,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他の猫も見る
+        Show other cat.
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
